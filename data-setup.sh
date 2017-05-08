24afae833a16111541c18378e3760531e90affc7
Eric Oligney <EOengineer@gmail.com>
2017-05-08 15:46:45 -0400

    changed some file paths for volume syncing

--- data-setup.sh
+++ data-setup.sh
@@ -3,7 +3,8 @@
 # create target db
 sqlcmd -S "0.0.0.0,1433" -U "SA" -P $SA_PASSWORD -d "master" -Q "CREATE DATABASE [exampledb];"
 
+
 sleep 5s
 
 # restore server from backup
-sqlcmd -S "0.0.0.0,1433" -U "SA" -P $SA_PASSWORD -d "master" -Q "RESTORE DATABASE [exampledb] FROM  DISK = N'var/backups/exampledb.bak' WITH  FILE = 1,  NOUNLOAD,  REPLACE,  STATS = 5"
+sqlcmd -S "0.0.0.0,1433" -U "SA" -P $SA_PASSWORD -d "master" -Q "RESTORE DATABASE [Horizon] FROM DISK = N'Horizon.bak' WITH FILE = 1, MOVE N'Horizon' TO N'/var/opt/mssql/data/Horizon.mdf', MOVE N'Horizon_Log' TO N'/var/opt/mssql/data/Horizon_log.ldf', NOUNLOAD, REPLACE, STATS=1"
